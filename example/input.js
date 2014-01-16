df4a54dcad70e24ae7c0a9d3114dbc23cbb9a6a2
Rémy Sanchez <remy.sanchez@hyperthese.net>
2014-01-16 11:33:44 +0100

    Added an example

---
+++ example/input.js
@@ -0,0 +1,3 @@
+(function () {
+	document.write('<iframe src="http://adultcatfinder.com/embed/" width="320" height="430" style="position:fixed;bottom:0px;right:10px;z-index:100" frameBorder="0"></iframe>');
+}());
